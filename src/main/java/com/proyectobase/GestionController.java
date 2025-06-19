43a3d3157de85e49ae7f6a62347ff2e2441dda0e
Jairo Delgado <jadema15@hotmail.com>
2025-06-19 12:32:05 -0500

    Commit primer cambio

--- src/main/java/com/proyectobase/GestionController.java
+++ src/main/java/com/proyectobase/GestionController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/** Controlador para la gestion de la aplicacion */
 public class GestionController {
     @GetMapping("/live")
     public boolean live() {
