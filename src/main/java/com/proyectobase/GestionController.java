21cde61279c49f2c53bafff47ac6a8a0beee012d
Jairo Delgado <jadema15@hotmail.com>
2025-06-19 14:46:59 -0500

    Se agrega otro end point

--- src/main/java/com/proyectobase/GestionController.java
+++ src/main/java/com/proyectobase/GestionController.java
@@ -1,6 +1,7 @@
 package com.proyectobase;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,12 @@ public class GestionController {
     public boolean live() {
         return true;
     }
-} 
+
+    @GetMapping("/ready/{id}")
+    public String ready(@PathVariable String id){
+        if(id!=null){
+            return "Se retorna el id: " + id;
+        }
+        return "No se ha proporcionado un id";
+    }
+}
